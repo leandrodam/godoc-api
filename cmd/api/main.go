de8888efa9809eaed484ea981f595a9162da3976
Leandro D. Alves <leandro.dalves@gmail.com>
2025-05-05 10:26:42 -0300

    feat: add main with echo web framework

---
+++ cmd/api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/leandrodam/godoc-api/internal/adapters/http/routes"
+	"github.com/leandrodam/godoc-api/internal/config"
+)
+
+func main() {
+	cfg := config.Load()
+
+	e := echo.New()
+
+	routes.RegisterRoutes(e)
+
+	log.Printf("starting server on port %s", cfg.Port)
+
+	if err := e.Start(cfg.Port); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
